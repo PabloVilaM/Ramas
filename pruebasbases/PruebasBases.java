c895f7e29757a671681356215b74f7b49ef69605
PabloVilaM <91198045+PabloVilaM@users.noreply.github.com>
2022-05-19 14:45:27 +0200

    Add files via upload

---
+++ pruebasbases/PruebasBases.java
@@ -0,0 +1,14 @@
+package pruebasbases;
+
+public class PruebasBases {
+
+    public static void main(String[] args) {
+        
+        GUI grafico = new GUI();
+        /*grafico.crearEventos();
+        grafico.crearTabla();*/
+        //grafico.insertar();
+        grafico.consultar();
+    }
+    
+}
